6d068976ffc545c067cfa8ac873d41059fbe6dd9
Jamal O'Garro <jamal.ogarro@gmail.com>
2014-12-10 22:36:52 -0500

    fixed code

--- public/main.js
+++ public/main.js
@@ -1,28 +1,54 @@
-var token = window.location.hash.replace("#access_token=", "");
-var access_token = { access_token: token };
+$(function() {
 
-function getImages(tag, access_token) {
-	var count = 12;
-	var url = 'https://api.instagram.com/v1/tags/' + tag + '/media/recent?callback=?&count=' + count;
-	
-	$.ajax({
-		type: 'GET',
-		url: url,
-		data: access_token,
-		success: loadData
-	});
+	var token = window.location.hash.replace("#access_token=", ""),
+		access_token = { access_token: token };
 
-};
+		// Makes the Instagram API call
+		function getImages(tag, access_token) {
+			
+			var url,
+				count = 12;
 
-function loadData(instagram_data) {
-	var photos,
-		photo,
-		new_image;
+				url = 'https://api.instagram.com/v1/tags/' + tag + '/media/recent?callback=?&count=' + count;
 
-		if (instagram_data.meta.code === 200){
-			photos = instagram_data.data;
-			consosle.log(instagram_data);
+				$.ajax({
+					type: 'GET',
+					url: url,
+					dataType: 'json',
+					data: access_token,
+					success: loadData
+				});
 		}
-}
 
-getImages("cars", access_token);
+		// Function that will actually append our images to the DOM
+		function loadData(instagram_data) {
+			console.log(instagram_data.data);
+			var photos,
+				photo,
+				new_image;
+
+				if (instagram_data.meta.code === 200){
+					photos = instagram_data.data;
+
+					for (i in photos){
+						photo = photos[i];
+
+						new_image = $('<img src="' +photo.images.standard_resolution.url + '">');
+						// $('#target').append(new_image);
+
+					new_image.load(function(){
+						$('#target').append($(this).hide());
+						   $(this).fadeIn(2000);
+						})
+					}
+				}
+
+		}
+
+		// add search functionality
+		$('#search-button').on("click", function(){
+			$tag = $('input.search-tag').val();
+			getImages($tag, access_token);
+		});
+
+});
