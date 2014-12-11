fa125028611af5abc6898a1e34935d4fa417706b
Jamal O'Garro <jamal.ogarro@gmail.com>
2014-12-10 20:50:52 -0500

    updatesd server

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
-var port = process.env.PORT || 8000;
+var port = process.env.PORT || 3000;
 app.listen(port);
