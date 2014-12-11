db70ebd7e36a3532014b3b122f488965e36c3477
Jamal O'Garro <jamal.ogarro@gmail.com>
2014-12-10 21:54:03 -0500

    updateds codes

--- public/main.js
+++ public/main.js
@@ -1,1 +1,28 @@
-alert('hello world!');
+var token = window.location.hash.replace("#access_token=", "");
+var access_token = { access_token: token };
+
+function getImages(tag, access_token) {
+	var count = 12;
+	var url = 'https://api.instagram.com/v1/tags/' + tag + '/media/recent?callback=?&count=' + count;
+	
+	$.ajax({
+		type: 'GET',
+		url: url,
+		data: access_token,
+		success: loadData
+	});
+
+};
+
+function loadData(instagram_data) {
+	var photos,
+		photo,
+		new_image;
+
+		if (instagram_data.meta.code === 200){
+			photos = instagram_data.data;
+			consosle.log(instagram_data);
+		}
+}
+
+getImages("cars", access_token);
