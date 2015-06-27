a11690224b0c60898e468bae4e139c7262178f7f
Lev Kravinsky <kravinskylev@gmail.com>
2015-06-27 14:14:01 -0400

    node server up

--- public/main.js
+++ public/main.js
@@ -1,54 +1,14 @@
-$(function() {
+var module = angular.module('sampleApp', []);
 
-	var token = window.location.hash.replace("#access_token=", ""),
-		access_token = { access_token: token };
+module.controller('mainController', function($scope) {
+  $scope.hello = "Hello, Angular";
 
-		// Makes the Instagram API call
-		function getImages(tag, access_token) {
-			
-			var url,
-				count = 12;
+  $scope.data = ["item1", "item2", "item3"];
+  $scope.logText = function(text) {
+    console.log(text);
+  };
 
-				url = 'https://api.instagram.com/v1/tags/' + tag + '/media/recent?callback=?&count=' + count;
-
-				$.ajax({
-					type: 'GET',
-					url: url,
-					dataType: 'json',
-					data: access_token,
-					success: loadData
-				});
-		}
-
-		// Function that will actually append our images to the DOM
-		function loadData(instagram_data) {
-			console.log(instagram_data.data);
-			var photos,
-				photo,
-				new_image;
-
-				if (instagram_data.meta.code === 200){
-					photos = instagram_data.data;
-
-					for (i in photos){
-						photo = photos[i];
-
-						new_image = $('<img src="' +photo.images.standard_resolution.url + '">');
-						// $('#target').append(new_image);
-
-					new_image.load(function(){
-						$('#target').append($(this).hide());
-						   $(this).fadeIn(2000);
-						})
-					}
-				}
-
-		}
-
-		// add search functionality
-		$('#search-button').on("click", function(){
-			$tag = $('input.search-tag').val();
-			getImages($tag, access_token);
-		});
-
+  $scope.alertMsg = function(text) {
+    alert(text);
+  };
 });
